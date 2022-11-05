6749263f99522f5f07be37c70a959f7849a7cfad
Yogan G <yogang2002@gmail.com>
2022-11-05 15:51:20 +0530

    Time: 3013 ms (52.00%), Space: 18.8 MB (8.40%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,62 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isWord = True
+        
+    def pruneWord(self, word) -> None:
+        cur: TrieNode = self
+        nodeAndChildKey: list[tuple[TrieNode, str]] = []
+        for char in word:
+            nodeAndChildKey.append((cur, char))
+            cur = cur.children[char]
+
+        for parentNode, childKey in reversed(nodeAndChildKey):
+            targetNode = parentNode.children[childKey]
+            if len(targetNode.children) == 0:
+                del parentNode.children[childKey]
+            else:
+                return
+
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+            
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = [], set()
+        
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or 
+                r == ROWS or c == COLS or
+                board[r][c] not in node.children or (r, c) in visit):
+                return
+            
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.append(word)
+                node.isWord = False
+                root.pruneWord(word)
+            
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visit.remove((r, c))
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, "")
+        
+        return res
